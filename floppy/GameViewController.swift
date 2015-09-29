be42211aa34dc78942f2cf2ff2edf0c45c5e7e7e
minhchau273 <minhchau273@gmail.com>
2015-09-29 20:14:18 +0700

    task 7

--- floppy/GameViewController.swift
+++ floppy/GameViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var birdView: UIImageView!
+    
+    var animator = UIDynamicAnimator()
+    var gravity = UIGravityBehavior()
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,10 +26,46 @@ class GameViewController: UIViewController {
         let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         self.view.backgroundColor = UIColor(patternImage: image)
+        
+        
+        animator.addBehavior(gravity)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func onStart(sender: UIButton) {
+        
+        gravity.addItem(birdView)
+    }
+    
+    
+    @IBAction func onTap(sender: UITapGestureRecognizer) {
+        
+        let birdItemBehavior = UIDynamicItemBehavior(items: [birdView])
+        
+        let initialVelocity = birdItemBehavior.linearVelocityForItem(birdView)
+        animator.addBehavior(birdItemBehavior)
+        print(initialVelocity)
+        
+        let push = UIPushBehavior(items: [birdView], mode: UIPushBehaviorMode.Instantaneous)
+        
+        push.pushDirection = CGVectorMake(0, -3)
+        push.active = true
+        animator.addBehavior(push)
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 }
