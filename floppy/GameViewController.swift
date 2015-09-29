186c2584805424125666c0246af10c10a6c45371
Harley Trung <harley@socialsci.com>
2015-09-29 19:50:43 +0700

    temp

--- floppy/GameViewController.swift
+++ floppy/GameViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        UIImage(named: "bg")?.drawInRect(self.view.bounds)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.view.backgroundColor = UIColor(patternImage: image)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
